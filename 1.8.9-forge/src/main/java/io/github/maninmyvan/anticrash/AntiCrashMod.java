8a3e5ed48e51f26e1aebd439b60cb9024b85e142
ManInMyVan <113963219+ManInMyVan@users.noreply.github.com>
2025-07-24 17:50:47 -0600

    simplify mod class

--- 1.8.9-forge/src/main/java/io/github/maninmyvan/anticrash/AntiCrashMod.java
+++ 1.8.9-forge/src/main/java/io/github/maninmyvan/anticrash/AntiCrashMod.java
@@ -1,14 +1,6 @@
 package io.github.maninmyvan.anticrash;
 
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.Instance;
 
-@Mod(modid = AntiCrashMod.MODID, name = AntiCrashMod.NAME, version = AntiCrashMod.VERSION)
-public class AntiCrashMod {
-    public static final String MODID = "@MOD_ID@";
-    public static final String NAME = "@MOD_NAME@";
-    public static final String VERSION = "@MOD_VERSION@";
-
-    @Instance(MODID)
-    public static AntiCrashMod INSTANCE;
-}
+@Mod(modid = "@MOD_ID@", name = "@MOD_NAME@", version = "@MOD_VERSION@")
+public class AntiCrashMod {}
